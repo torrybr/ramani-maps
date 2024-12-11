5062054b0f3165a4af0bb03c7ae98c862ca7cfd0
torrybr <16907963+torrybr@users.noreply.github.com>
2024-12-11 17:30:42 +0000

    chore: update deprecated LocalLifecycleObserver

--- ramani-mapbox/src/main/java/org/ramani/compose/Utils.kt
+++ ramani-mapbox/src/main/java/org/ramani/compose/Utils.kt
@@ -10,23 +10,24 @@
 
 package org.ramani.compose
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import com.mapbox.maps.MapView
-import com.mapbox.maps.MapboxMap
-import com.mapbox.maps.ScreenCoordinate
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import org.maplibre.android.MapLibre
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.MapView
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
-import kotlin.math.hypot
 
 @Composable
 fun rememberMapViewWithLifecycle(): MapView {
     val context = LocalContext.current
+    MapLibre.getInstance(context)
     val mapView = remember { MapView(context) }
     val lifecycle = LocalLifecycleOwner.current.lifecycle
 
@@ -41,10 +42,14 @@ fun rememberMapViewWithLifecycle(): MapView {
     return mapView
 }
 
+
 fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver {
     return LifecycleEventObserver { _, event ->
         when (event) {
+            Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
             Lifecycle.Event.ON_START -> mapView.onStart()
+            Lifecycle.Event.ON_RESUME -> mapView.onResume()
+            Lifecycle.Event.ON_PAUSE -> mapView.onPause()
             Lifecycle.Event.ON_STOP -> mapView.onStop()
             Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
             else -> throw IllegalStateException()
@@ -52,23 +57,9 @@ fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver {
     }
 }
 
-suspend inline fun MapView.awaitMap(): MapboxMap =
+suspend inline fun MapView.awaitMap(): MapLibreMap =
     suspendCoroutine { continuation ->
-        continuation.resume(this.getMapboxMap())
+        getMapAsync {
+            continuation.resume(it)
+        }
     }
-
-operator fun ScreenCoordinate.plus(other: ScreenCoordinate): ScreenCoordinate {
-    return ScreenCoordinate(this.x + other.x, this.y + other.y)
-}
-
-operator fun ScreenCoordinate.minus(other: ScreenCoordinate): ScreenCoordinate {
-    return ScreenCoordinate(this.x - other.x, this.y - other.y)
-}
-
-operator fun ScreenCoordinate.times(scalar: Double): ScreenCoordinate {
-    return ScreenCoordinate(this.x * scalar, this.y * scalar)
-}
-
-fun ScreenCoordinate.length(): Float {
-    return hypot(this.x, this.y).toFloat()
-}
